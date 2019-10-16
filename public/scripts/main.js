faf1f6743fd34a06f32090a82bcc02b85fa58662
kazumoto <motch0214@gmail.com>
2019-10-16 17:34:16 +0900

    Write messages to Cloud Firestore

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -51,7 +51,15 @@ function isUserSignedIn() {
 
 // Saves a new message on the Firebase DB.
 function saveMessage(messageText) {
-  // TODO 7: Push a new message to Firebase.
+  // Add a new message entry to the database.
+  return firebase.firestore().collection('messages').add({
+    name: getUserName(),
+    text: messageText,
+    profilePicUrl: getProfilePicUrl(),
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+  }).catch(function(error) {
+    console.error('Error writing new message to database', error);
+  });
 }
 
 // Loads chat messages history and listens for upcoming ones.
